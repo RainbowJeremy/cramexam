ecbb8a5cb13c67b0ea051a8b3d6b2660fd3b9522
Fintan <fintanjparsons@gmail.com>
2021-07-24 16:57:36 +0100

    all gone

--- mcqgenerator/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class McqgeneratorConfig(AppConfig):
-    name = 'mcqgenerator'
-
-    def ready(self):
-        import mcqgenerator.signals
--- surveys/admin.py
+++
@@ -1,6 +0,0 @@
-from surveys.models import Survey
-from django.contrib import admin
-from .models import Newsletter, Survey
-# Register your models here.
-admin.site.register(Survey)
-admin.site.register(Newsletter)
